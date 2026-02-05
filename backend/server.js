3bd42bc2e205b2ebfe7193aab287c6e6fa7e9469
Dharaheshh <dharaheshh@gmail.com>
2026-02-06 01:21:56 +0530

    proper cors fix

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: ["https://mini-project-i-six.vercel.app"],
+  credentials: true
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

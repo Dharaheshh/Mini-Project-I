813daddf0efcbf6b10f26b6b5ccc165144a9ad6c
Dharaheshh <dharaheshh@gmail.com>
2026-06-04 22:05:21 +0530

    test

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -15,6 +15,7 @@ const auth = async (req, res, next) => {
     if (!user) {
       return res.status(401).json({ message: 'Token is not valid' });
     }
+    //changed
 
     req.user = user;
     next();
